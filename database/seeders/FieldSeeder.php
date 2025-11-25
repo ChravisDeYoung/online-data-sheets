7314f1f701ac963f5719bcbdf30c872fc6ebd224
Travis De Jong <dejong_travis@yahoo.ca>
2025-11-25 13:20:06 -0500

    adjust field seeder

--- database/seeders/FieldSeeder.php
+++ database/seeders/FieldSeeder.php
@@ -11,29 +11,23 @@ class FieldSeeder extends Seeder
 {
     public function run()
     {
-        $subsections = [
-            1 => 'General',
-            2 => 'Contact',
-            3 => 'Equipment',
-            4 => 'Maintenance',
-            5 => 'Other',
-        ];
+        $subsectionOptions = ['General', 'Contact', 'Equipment', 'Maintenance', 'Other'];
 
         foreach (Page::all() as $page) {
-            $subsectionRand = array_rand($subsections);
+            $subsectionSortOrder = 1;
+            $subsections = Arr::random($subsectionOptions, random_int(1, count($subsectionOptions)));
 
-            // generate columns
-            $allNumbers = range(1, $page->column_count);
-            $countToSelect = random_int(1, $page->column_count);
-            $selectedNumbers = Arr::random($allNumbers, $countToSelect);
+            foreach ($subsections as $subsection) {
+                $selectedNumbers = Arr::random(range(1, $page->column_count), random_int(1, $page->column_count));
 
-            Field::factory(random_int(5, 25))
-                ->for($page)
-                ->create([
-                    'subsection' => $subsections[$subsectionRand],
-                    'subsection_sort_order' => $subsectionRand,
-                    'required_columns' => implode(',', $selectedNumbers),
-                ]);
+                Field::factory(random_int(3, 10))
+                    ->for($page)
+                    ->create([
+                        'subsection' => $subsection,
+                        'subsection_sort_order' => $subsectionSortOrder++,
+                        'required_columns' => implode(',', $selectedNumbers),
+                    ]);
+            }
         }
     }
 }
