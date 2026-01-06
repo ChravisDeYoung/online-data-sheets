5939f86bca52300d1af22e55715ce5278b8625c7
Travis De Jong <dejong_travis@yahoo.ca>
2026-01-06 13:36:57 -0500

    add ids to inputs to resolve errors

--- README.md
+++ README.md
@@ -31,10 +31,14 @@ This is a project to learn the new stack with php@7.4 and laravel@8
 - timestamped deletions (only delete in the first few minutes)
 - Laravel breeze has authentication
 - laravel dusk testing
-- github actions
+- github actions (testing on push)
 - calculation fields
+- conditional fields
 - chart fields (graph over time)
 - report for date range
 - api sync with external systems
-- different page styles
+- different page styles (row and column)
 - subscribe to pages
+- custom subscriptions for fields and pages (ex. custom range or % out of range on a page)
+- support multiple organizations
+  - timezones, logos, colours?
--- resources/views/components/data/input.blade.php
+++ resources/views/components/data/input.blade.php
@@ -10,24 +10,25 @@
     @switch ($field->type)
         @case(Field::TYPE_TEXT)
             <input type="text" onblur="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
-                   value="{{ $value }}"
+                   value="{{ $value }}" id="{{ $field->id }}-{{ $column }}"
                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             @break
         @case(Field::TYPE_NUMBER)
             <input type="number" onblur="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
-                   value="{{ $value }}"
+                   value="{{ $value }}" id="{{ $field->id }}-{{ $column }}"
                    class=" {{ optional($fieldData)->is_out_of_range ? 'out-of-range' : '' }} bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             @break
         @case(Field::TYPE_DATE)
             <input type="date" onblur="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
-                   value="{{ $value }}"
+                   value="{{ $value }}" id="{{ $field->id }}-{{ $column }}"
                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             @break
         @case(Field::TYPE_SELECT)
             <select onblur="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
+                    id="{{ $field->id }}-{{ $column }}"
                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option value="">Select an option</option>
                 @foreach(explode(',', $field->select_options) as $option)
@@ -38,13 +39,14 @@
             </select>
             @break
         @case(Field::TYPE_CHECKBOX)
-            <input type="checkbox" value=""
+            <input type="checkbox" value="" id="{{ $field->id }}-{{ $column }}"
                    onclick="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
                    {{ filter_var($value, FILTER_VALIDATE_BOOLEAN) ? 'checked' : '' }}
                    class="w-6 h-6 text-blue-600 bg-gray-50 border-gray-300 rounded-md focus:ring-blue-600 dark:focus:ring-blue-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             @break
         @case(Field::TYPE_TEXTAREA)
             <textarea onblur="saveFieldData(this, {{ $field->id }}, {{ $column }}, '{{ $date }}')"
+                      id="{{ $field->id }}-{{ $column }}"
                       class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{ $value }}</textarea>
             @break
     @endswitch
